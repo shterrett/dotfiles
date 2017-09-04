c1072778d40c4885aba2b30f4beff57c6707fe5d
Stuart Terrett <shterrett@gmail.com>
2017-09-04 19:13:01 -0400

    More Arch Linux

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,13 @@
 import XMonad
 import Solarized
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 
 main = do
     xmonad defaultConfig {
        normalBorderColor = solarizedBase01
      , focusedBorderColor = solarizedRed
      , terminal = "xterm"
+     , manageHook = manageDocks <+> manageHook defaultConfig
+     , layoutHook = avoidStruts $ layoutHook defaultConfig
     }
