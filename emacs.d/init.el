1a485c6c7f54931c05ebe53640f2383aed2b35d6
Stuart Terrett <shterrett@gmail.com>
2019-10-22 22:11:28 -0400

    Emacs for Agda

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -142,3 +142,20 @@
 (evil-mode 1)
 (require 'evil-surround)
 (global-evil-surround-mode 1)
+
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
+
+;; agda mode
+(setq auto-mode-alist
+   (append
+     '(("\\.agda\\'" . agda2-mode)
+       ("\\.lagda.md\\'" . agda2-mode))
+     auto-mode-alist))
+
+default to mononoki
+(set-face-attribute 'default nil
+    :family "mononoki"
+    :height 120
+    :weight 'normal
+    :width  'normal)
